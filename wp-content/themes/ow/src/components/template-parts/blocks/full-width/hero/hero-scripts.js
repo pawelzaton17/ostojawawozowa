3885852ecf078452d494a6b31e45bf3032e2f492
Krzysztof Majstrowicz <krzysztof.majstrowicz@chop-chop.org>
2021-08-18 21:20:08 +0200

    Block Hero in progress (desktop's ready)

---
+++ wp-content/themes/ow/src/components/template-parts/blocks/full-width/hero/hero-scripts.js
@@ -0,0 +1,55 @@
+/**
+ * ACF Block Hero Template Part Scripts
+ *
+ */
+
+/** Tiny Slider */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "tiny-slider/dist/tiny-slider.css";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { tns } from "tiny-slider/src/tiny-slider";
+
+/** Init Tiny Slider with dots and custom controls */
+
+const blockHeroSlider = () => {
+    const commonCarouselWrapper = document.querySelectorAll(".js-tiny-slider-hero");
+
+    [...commonCarouselWrapper].forEach((sliderWrapper) => {
+        const slider = sliderWrapper.querySelector(".js-tiny-slider-row");
+        const controlsContainer = sliderWrapper.querySelector(".crunch-tiny-slider__controls");
+        const tnsSlider = tns({
+            container: slider,
+            items: 1,
+            autoplay: true,
+            mouseDrag: false,
+            lazyload: true,
+            nav: true,
+            navPosition: "bottom",
+            loop: false,
+            controls: true,
+            mode: "gallery",
+            animateDelay: 300,
+            controlsText: [
+                "<svg class='tns-controls__icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 253.21 442.83'><title>icon__chevron-regular-left--black-color</title><path d='M229.9,439.31l19.8-19.79a12,12,0,0,0,0-17L69,221.41,249.7,40.28a12,12,0,0,0,0-17L229.9,3.51a12,12,0,0,0-17,0L3.51,212.93a12,12,0,0,0,0,17L212.93,439.31A12,12,0,0,0,229.9,439.31Z'/></svg>",
+                "<svg class='tns-controls__icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 253.21 442.83'><title>icon__chevron-regular-right--black-color</title><path d='M23.31,3.52,3.51,23.31a12,12,0,0,0,0,17l180.7,181.13L3.51,402.54a12,12,0,0,0,0,17l19.8,19.79a12,12,0,0,0,17,0L249.7,229.9a12,12,0,0,0,0-17L40.28,3.52A12,12,0,0,0,23.31,3.52Z'/></svg>",
+            ],
+            controlsContainer,
+        });
+        // Slider counter
+        let sliderInfo = tnsSlider.getInfo();
+        document.querySelector(
+            ".slider-info-print"
+        ).innerHTML = `${sliderInfo.slideBy} / ${sliderInfo.slideCount}`;
+
+        tnsSlider.events.on("indexChanged", () => {
+            sliderInfo = tnsSlider.getInfo();
+            document.querySelector(".slider-info-print").innerHTML = `${sliderInfo.index + 1} / ${
+                sliderInfo.slideCount
+            }`;
+        });
+    });
+};
+
+if (document.querySelector(".js-tiny-slider")) {
+    blockHeroSlider();
+}
---
+++ wp-content/themes/ow/template-parts/blocks/full-width/hero.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Hero Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'acf-block-hero-' . $block['id'];
+if (!empty($block['anchor'])) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" values.
+$class_name       = 'acf-block-hero';
+$additional_class = !empty($block['className']) ? ' ' .$block['className'] : null;
+
+// Load values and assing defaults.
+//$title = get_field( 'title' );
+
+// block preview
+if (!empty($block['data']['__is_preview'])) : ?>
+<img src="<?= get_template_directory_uri(); ?>/inc/block-previews/<?= $class_name; ?>.jpg" />
+
+<?php return;
+endif; ?>
+
+
+<?php
+/**
+ * Begin Custom Block
+ */
+do_action('container_start');
+?>
+
+<section id="<?= esc_attr($id); ?>" class="<?= esc_attr($class_name.$additional_class); ?>">
+
+    <?php if ( have_rows( 'slider' ) ): ?>
+
+    <div class="js-tiny-slider-hero">
+        <div class="js-tiny-slider crunch-tiny-slider position-relative">
+            <div class="js-tiny-slider-row">
+
+            <?php
+            while ( have_rows( 'slider' ) ) : the_row();
+                $title     = get_sub_field( 'title' );
+                $image_id  = get_sub_field( 'image' );
+                $link      = get_sub_field( 'link' );
+                $image_url = '';
+
+                if ( $link ) {
+                    $link_url    = $link['url'];
+                    $link_title  = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                }
+                if ( $image_id ) {
+                    $image_url = wp_get_attachment_image_url( $image_id, 'full' );
+                    $style     = "style='background-image: url({$image_url})'";
+                }
+            ?>
+
+                <div class="acf-block-hero__slider-item tiny-slider-item">
+                    <figure class="h-100 w-100 background-cover" <?= $style?>></figure>
+                    <div class="acf-block-hero__content-wrapper position-absolute c-p-7 c-pr-10">
+                        <div class="acf-block-hero__title text-white c-mb-7">
+
+                            <?= $title; ?>
+
+                        </div>
+                        <a href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>" <?php if($link_target != '_self') echo 'rel="'.esc_attr('nofollow').'"'; ?> class="crunch-button crunch-button__full-background crunch-button__full-background--secondary-color text-decoration-none font-family-primary">
+
+                            <?= esc_html($link_title); ?>
+
+                        </a>
+                    </div>
+                </div>
+
+            <?php endwhile; ?>
+
+            </div>
+            <div class="acf-block-hero__controls d-none d-md-block">
+                <div class="container d-flex align-items-center h-100">
+                    <div class="row w-100">
+                        <div class="col-10">
+                            <div class="text-white slider-info-print font-size-15 font-weight-medium"></div>
+                        </div>
+                        <div class="col-2 position-relative">
+                            <div class="crunch-tiny-slider__controls d-flex align-items-center h-100 js-crunch-tiny-slider-custom-controls">
+                                <button type="button" class="acf-block-hero__control-item crunch-tiny-slider__control-button crunch-tiny-slider-button-prev hover-opacity-0-75 bg-transparent border-0">
+                                    <svg class="acf-block-hero__control-icon crunch-tiny-slider__control-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 253.21 442.83"><title>icon__chevron-regular-left--black-color</title><path d="M229.9,439.31l19.8-19.79a12,12,0,0,0,0-17L69,221.41,249.7,40.28a12,12,0,0,0,0-17L229.9,3.51a12,12,0,0,0-17,0L3.51,212.93a12,12,0,0,0,0,17L212.93,439.31A12,12,0,0,0,229.9,439.31Z"/></svg>
+                                </button>
+                                <button type="button" class="acf-block-hero__control-item crunch-tiny-slider__control-button crunch-tiny-slider-button-next hover-opacity-0-75 bg-transparent border-0">
+                                    <svg class="acf-block-hero__control-icon crunch-tiny-slider__control-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 253.21 442.83"><title>icon__chevron-regular-right--black-color</title><path d="M23.31,3.52,3.51,23.31a12,12,0,0,0,0,17l180.7,181.13L3.51,402.54a12,12,0,0,0,0,17l19.8,19.79a12,12,0,0,0,17,0L249.7,229.9a12,12,0,0,0,0-17L40.28,3.52A12,12,0,0,0,23.31,3.52Z"/></svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php endif; ?>
+
+</section>
+
+<?php
+/**
+ * End Custom Block
+ */
+do_action('container_end');
+?>
