47d142f7509f559f3d642d5c191cb3870e0f4e1b
Pawel Zatoń <pawzatkontakt@gmail.com>
2021-08-18 22:50:43 +0200

    WP Structure

--- wp-content/themes/ow/template-parts/blocks/full-width/block-numbers.php
+++ wp-content/themes/ow/template-parts/blocks/full-width/block-numbers.php
@@ -20,7 +20,8 @@ $class_name = 'acf-block-numbers';
 $additional_class = !empty($block['className']) ? ' ' .$block['className'] : null;
 
 // Load values and assing defaults.
-// $title = get_field( 'title' );
+$v_title = get_field( 'v_title' );
+$w_title = get_field( 'w_title' );
 
 // block preview
 if (!empty($block['data']['__is_preview'])) : ?>
@@ -38,6 +39,87 @@ do_action('container_start');
 ?>
 
 <section id="<?= esc_attr($id); ?>" class="<?= esc_attr($class_name.$additional_class); ?>">
+    <div class="container">
+        <div class="row">
+
+        <?php if ( have_rows( 'v_items' ) ): ?>
+
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-8">
+                        <h2 class="acf-block-numbers__mainheading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
+
+                            <?= $v_title; ?>
+
+                        </h2>
+                    </div>
+
+                    <?php  
+                    while ( have_rows( 'v_items' ) ): the_row();
+                        $v_heading     = get_sub_field( 'v_item_heading' );
+                        $v_content     = get_sub_field( 'v_item_content' );
+                    ?>
+
+                    <div class="col-6">
+                        <h2 class="acf-block-numbers__heading">
+
+                            <?= $v_heading; ?>
+
+                        </h2>
+                        <div class="acf-block-numbers__content">
+
+                            <?= $v_content; ?>
+
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                
+                </div>
+            </div>
+            
+            <?php endif; ?>
+
+            <?php if ( have_rows( 'w_items' ) ): ?>
+
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="acf-block-numbers__secondaryheading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
+
+                            <?= $w_title; ?>
+
+                        </h2>
+                    </div>
+
+                    <?php  
+                    while ( have_rows( 'w_items' ) ): the_row();
+                        $w_heading     = get_sub_field( 'w_item_heading' );
+                        $w_content     = get_sub_field( 'w_item_content' );
+                    ?>
+
+                    <div class="col-6">
+                        <h2 class="acf-block-numbers">
+
+                            <?= $w_heading; ?>
+
+                        </h2>
+                        <div class="acf-block-numbers">
+
+                            <?= $w_content; ?>
+
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                
+                </div>
+            </div>
+            
+            <?php endif; ?>
+
+        </div>
+    </div> 
 </section>
 
 <?php
