5cd1318eadc536b96f20de48ce5d45c3e4c0ca2d
Pawel Zatoń <pawzatkontakt@gmail.com>
2021-08-19 12:11:31 +0200

    Couple fixes

--- wp-content/themes/ow/template-parts/blocks/full-width/block-numbers.php
+++ wp-content/themes/ow/template-parts/blocks/full-width/block-numbers.php
@@ -47,7 +47,7 @@ do_action('container_start');
             <div class="col-lg-6">
                 <div class="row">
                     <div class="col-8">
-                        <h2 class="acf-block-numbers__mainheading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
+                        <h2 class="acf-block-numbers__left-heading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
 
                             <?= $v_title; ?>
 
@@ -78,14 +78,15 @@ do_action('container_start');
                 </div>
             </div>
             
-            <?php endif; ?>
+            <?php 
+            endif;
 
-            <?php if ( have_rows( 'w_items' ) ): ?>
+            if ( have_rows( 'w_items' ) ): ?>
 
             <div class="col-lg-6">
                 <div class="row">
                     <div class="col-12">
-                        <h2 class="acf-block-numbers__secondaryheading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
+                        <h2 class="acf-block-numbers__right-heading c-pb-4 c-pb-md-3 position-relative font-weight-bold font-size-36">
 
                             <?= $w_title; ?>
 
