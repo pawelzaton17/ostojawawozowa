66afae36dd48c4e8f89ddb41f0b1d1776f3c7c22
Krzysztof Majstrowicz <krzysztof.majstrowicz@chop-chop.org>
2021-09-19 20:02:36 +0200

    Archive page fixed

--- wp-content/themes/ow/page.php
+++ wp-content/themes/ow/page.php
@@ -11,10 +11,16 @@ defined('ABSPATH') || exit;
 
 get_header();
 the_post();
+
+$is_archive_page = get_field( 'page_archive' );
 ?>
 
 <main id="main">
+
+    <?= $is_archive_page ? get_template_part('template-parts/components/archive') : null; ?>
+
     <?php remove_empty_nodes_from_content(get_the_content()); ?>
+
 </main>
 
 <?php
