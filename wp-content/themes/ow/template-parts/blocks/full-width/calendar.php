9b4048c2ba48e5a52d2c1609464595949533d74c
Krzysztof Majstrowicz <krzysztof.majstrowicz@chop-chop.org>
2021-09-25 08:31:56 +0200

    Block Calendar | New modals added

--- wp-content/themes/ow/template-parts/blocks/full-width/calendar.php
+++ wp-content/themes/ow/template-parts/blocks/full-width/calendar.php
@@ -127,13 +127,13 @@ do_action('container_start');
                         <div class="acf-block-calendar__buttons-wrapper c-px-6 c-px-md-0 c-pb-md-5 c-mt-5 c-mt-md-0 d-flex flex-column flex-md-row justify-content-md-center">
                             <button
                                 class="js-modal-trigger acf-block-calendar__modal-button acf-block-calendar__modal-button--short font-size-14 border-0 crunch-button crunch-button__full-background crunch-button__full-background--medium text-decoration-none font-family-primary text-primary w-100"
-                                data-target-modal="#"
+                                data-target-modal="#calendar-other-date-modal"
                             >
                                 Inny termin
                             </button>
                             <button
                                 class="js-modal-trigger acf-block-calendar__modal-button acf-block-calendar__modal-button--long font-size-14 border-0 crunch-button crunch-button__full-background crunch-button__full-background--medium text-decoration-none font-family-primary text-black w-100"
-                                data-target-modal="#"
+                                data-target-modal="#calendar-without-date-modal"
                             >
                                 Umów się
                             </button>
@@ -156,6 +156,36 @@ do_action('container_start');
             <div class="h-100 overflow-auto">
 
                 <?= do_shortcode('[gravityform id="11" ajax="true"]'); ?>
+
+            </div
+        </div>
+    </div>
+    <div class="js-modal modal contact-modal-wrapper fade" id="calendar-other-date-modal">
+        <div class="main-footer__popup-wrapper js-modal-item m-auto overflow-hidden bg-white h-100 position-relative c-py-4 c-px-6">
+            <i class="js-modal-close modal__close z-index-2 position-absolute c-p-3 d-block">
+                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M27.9566 27.9568L10.0433 10.0434" stroke="#333333" stroke-width="1.2" stroke-linecap="round"/>
+                    <path d="M27.9567 10.0434L10.0434 27.9568" stroke="#333333" stroke-width="1.2" stroke-linecap="round"/>
+                </svg>
+            </i>
+            <div class="h-100 overflow-auto">
+
+                Inny termin
+
+            </div
+        </div>
+    </div>
+    <div class="js-modal modal contact-modal-wrapper fade" id="calendar-without-date-modal">
+        <div class="main-footer__popup-wrapper js-modal-item m-auto overflow-hidden bg-white h-100 position-relative c-py-4 c-px-6">
+            <i class="js-modal-close modal__close z-index-2 position-absolute c-p-3 d-block">
+                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M27.9566 27.9568L10.0433 10.0434" stroke="#333333" stroke-width="1.2" stroke-linecap="round"/>
+                    <path d="M27.9567 10.0434L10.0434 27.9568" stroke="#333333" stroke-width="1.2" stroke-linecap="round"/>
+                </svg>
+            </i>
+            <div class="h-100 overflow-auto">
+
+                Umów się
 
             </div
         </div>
