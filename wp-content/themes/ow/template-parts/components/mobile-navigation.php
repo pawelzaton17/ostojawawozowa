5ed9be1d80ea57de0620b378107491d86455becd
Krzysztof Majstrowicz <krzysztof.majstrowicz@chop-chop.org>
2021-09-29 22:13:40 +0200

    Menu | Nav logo added

--- wp-content/themes/ow/template-parts/components/mobile-navigation.php
+++ wp-content/themes/ow/template-parts/components/mobile-navigation.php
@@ -1,7 +1,20 @@
+<?php
+$menu_logo_id = get_field( 'h_menu_logo', 'options' );
+?>
 <nav id="mobile-navigation" class="js-mobile-navigation">
     <div id="subpanel" class="panel">
 
+        <?php if ( ! empty( $menu_logo_id ) ) : ?>
+
+        <figure class="mobile-navigation__logo position-absolute c-mb-0">
+
+            <?= wp_get_attachment_image( $menu_logo_id, "full", "", array( "class" => "d-block h-auto w-100 lazyload", "data-lazy" => "true") ); ?>
+
+        </figure>
+
         <?php
+        endif;
+
         wp_nav_menu(
             array(
                 'container' => false,
